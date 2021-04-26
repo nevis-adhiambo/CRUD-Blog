009fbf072a51faf137b50a2101626ad5d77c7a89
Israel Abraham <israelvictory87@gmail.com>
2021-04-26 20:17:56 +0100

    Implemented LoginRequiredMixins and login_required decorator for some pages

--- blog/views.py
+++ blog/views.py
@@ -4,31 +4,40 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
 from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class BlogCreateView(CreateView):
+
+class BlogCreateView(CreateView, LoginRequiredMixin):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
 
-class BlogUpdateView(UpdateView):
+
+class BlogUpdateView(UpdateView, LoginRequiredMixin):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
 
-class BlogDeleteView(DeleteView):
+
+class BlogDeleteView(DeleteView, LoginRequiredMixin):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
 
+
+@login_required(login_url="/accounts/login/")
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
