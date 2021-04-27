d1322213aef1b89471c6bce5f44434e369d8450f
Nevis <nevisadhiambo71@gmail.com>
2021-04-27 11:46:22 +0300

    import heroku to settings file

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "Nevis is pretty"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["127.0.0.1", "safe-retreat-09681.herokuapp.com"]
